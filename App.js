047b174acb22d3d334ed8d56fd467d0b870df52b
DeviprasannaG <110509065+DeviprasannaG@users.noreply.github.com>
2022-10-25 16:11:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+//import Card from './Card'
+function App(){
+  const [search,setSearch] = useState('');
+  const [searchresults, setSearchresults]= useState([]);
+  
+  async function apiSubmit(){
+  const response =await axios.get('https://api.nytimes.com/svc/books/v3/reviews.json?author='+search+'&api-key=1M4wJOpIzU95fnG4f5VuizAy2IGA5AnV');
+  console.log(response.data);
+  setSearchresults(response.data);
+  }
+  
+  return (
+  <div className="App">
+             <input type="text" value={search} onChange={(e) =>{setSearch(e.target.value)}}/>
+               <button type="submit"  onClick={(e)=>{apiSubmit()}} className="bth bth-success">Submit</button>   
+  {
+    searchresults &&
+    <div>
+      <h1>{searchresults.status}</h1>
+  </div>
+}</div>
+  )}
+
+export default App;
